f9c6bc4a1d17e5aab1f0ebba09159ff937605d84
Simon Weizman <simon@weizman.us>
2021-02-17 19:00:13 -0500

    Implemented Unit testing for Flask & Mongoengine, and updated backend-specific README

---
+++ backend/python/tests/test_hackers.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+import json
+from src.models.hacker import Hacker
+from tests.base import BaseTestCase
+
+
+class TestHackersBlueprint(BaseTestCase):
+    """Tests for the Hackers Endpoints"""
+    pass
