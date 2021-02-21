b6f500cd7f1f4c20384f7e7b623b6519ee05b070
Simon Weizman <simon@weizman.us>
2021-02-20 23:28:28 -0500

    Created Club Events Endpoint & Model. Need to authorize who is accessing the endpoint.

---
+++ backend/python/src/api/club_events.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+    src.api.club_events
+    ~~~~~~~~~~~~~~~~~~~
+
+    Functions:
+
+        update_events()
+
+"""
+from flask import Blueprint, request
+from mongoengine.errors import ValidationError
+from werkzeug.exceptions import BadRequest, Unauthorized
+import dateutil.parser
+from src.models.club_event import ClubEvent
+
+club_events_blueprint = Blueprint("club_events", __name__)
+
+
+@club_events_blueprint.route("/club/update_events/", methods=["PUT"])
+def update_events():
+    """
+    Updates the Club Events.
+    ---
+    tags:
+        - club
+    security:
+        - ApiKeyAuth: []
+    summary: Update Club Events
+    requestBody:
+        content:
+            application/json:
+                schema:
+                    type: array
+                    items:
+                        $ref: '#/components/schemas/ClubEvent'
+        required: true
+    responses:
+        201:
+            description: OK
+        400:
+            description: Bad request.
+        401:
+            description: Invalid or Missing API Key.
+        5XX:
+            description: Unexpected error.
+    """
+    data = request.get_json()
+
+    if not data:
+        raise BadRequest()
+
+    auth_token = request.headers.get("Authorization")
+    if not auth_token:
+        raise Unauthorized()
+
+    # TODO: Authorize the thing
+
+    ClubEvent.drop_collection()
+
+    for event in data:
+
+        if event["date"]:
+            event["date"] = dateutil.parser.parse(event["date"])
+
+        try:
+            ClubEvent.createOne(**event)
+        except ValidationError:
+            raise BadRequest()
+
+    res = {
+        "status": "success",
+        "message": "Events successfully updated!"
+    }
+
+    return res, 201
