05856efa7193b08c70c858cadfcbb44a14dc7a28
AdamPellot <59068040+AdamPellot@users.noreply.github.com>
2021-02-21 19:18:15 -0500

    Update hackers.py

--- backend/python/src/api/hackers.py
+++ backend/python/src/api/hackers.py
@@ -75,6 +75,88 @@ def create_hacker():
 
     return res, 201
 
+@hackers_blueprint.route("/hackers/", methods=["POST"])
+def create_hacker():
+    """
+    Creates a new Hacker.
+    ---
+    tags:
+        - hacker
+    summary: Create Hacker
+    requestBody:
+        content:
+            application/json:
+                schema:
+                    $ref: '#/components/schemas/Hacker'
+        description: Created Hacker Object
+        required: true
+    responses:
+        201:
+            description: OK
+        400:
+            description: Bad request.
+        409:
+            description: Sorry, that username or email already exists.
+        5XX:
+            description: Unexpected error.
+    """
+    data = request.get_json()
+
+    if not data:
+        raise BadRequest()
+
+    if data["date"]:
+        data["date"] = dateutil.parser.parse(data["date"])
+
+    data["hacker_profile"] = {}
+    for f in HACKER_PROFILE_FIELDS:
+        data["hacker_profile"][f] = data.pop(f, None)
+
+    try:
+        Hacker.createOne(**data, roles=("HACKER",))
+    except NotUniqueError:
+        raise Conflict("Sorry, that username or email already exists.")
+    except ValidationError:
+        raise BadRequest()
+
+    res = {
+        "status": "success",
+        "message": "Hacker was created!"
+    }
+
+    return res, 201
+
+@hackers_blueprint.route("/hackers/<username>/", methods=["GET"])
+def get_user_search(username: str):
+    """
+    Retrieves a hacker's profile using their username.
+    ---
+    tags:
+        - hacker
+    summary: Gets a hacker's profile from their username.
+    parameters:
+        - name: username
+          in: path
+          type: string
+          description: The hacker's profile.
+          required: true
+    responses:
+        200:
+            description: OK
+
+    """
+    hacker = Hacker.objects(username=username).first()
+    if not hacker:
+        raise NotFound()
+
+    res = {
+        "Hacker Profile": hacker.hacker_profile,
+        "User Name": hacker.username,
+        "message": "Successfully reached profile.",
+        "status": "success"
+    }
+
+    return res, 200
 
 @hackers_blueprint.route("/hackers/<username>/", methods=["DELETE"])
 def delete_hacker(username: str):
