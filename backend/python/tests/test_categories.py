ca248ee3bc493bf0968442616787ea77e1abfad5
Simon Weizman <simon@weizman.us>
2021-02-24 19:13:14 -0500

    Added new tests

---
+++ backend/python/tests/test_categories.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+import json
+from src.models.category import Category
+from tests.base import BaseTestCase
+
+
+class TestCategoriesBlueprint(BaseTestCase):
+    """Tests for the categories Endpoints"""
+    pass
---
+++ backend/python/tests/test_club_events.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+import json
+from src.models.club_event import ClubEvent
+from tests.base import BaseTestCase
+
+
+class TestClubEventsBlueprint(BaseTestCase):
+    """Tests for the Club Events Endpoints"""
+    pass
