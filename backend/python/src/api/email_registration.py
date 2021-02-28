c9d686f65613809881028b0220fbedcf684e039b
Simon Weizman <simon@weizman.us>
2021-02-27 22:53:14 -0500

    Added blank send verification email route

--- backend/python/src/api/email_registration.py
+++ backend/python/src/api/email_registration.py
@@ -74,7 +74,7 @@ def update_registration_status(email_token: str):
     user = User.objects(id=user_username).first()
 
     if not user:
-        raise NotFound()
+        raise NotFound("Invalid verification token. Please try again.")
 
     user.update(email_registration=True)
 
@@ -84,3 +84,33 @@ def update_registration_status(email_token: str):
     }
 
     return res, 200
+
+
+@email_reg_blueprint.route("/email_registration/send/<username>/", methods=["POST"])  # noqa: E501
+def send_registration_email(username: str):
+    """
+    Sends a registration email to the user.
+    ---
+    tags:
+        - email_registration
+    parameters:
+        - name: username
+          in: path
+          schema:
+            type: string
+          required: true
+    responses:
+        201:
+            description: OK
+        404:
+            description: No User exists with that username!
+        5XX:
+            description: Unexpected error.
+    """
+
+    res = {
+        "status": "success",
+        "message": "Verification email successfully sent!"
+    }
+
+    return res, 201
