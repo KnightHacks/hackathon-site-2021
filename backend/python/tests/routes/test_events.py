f65b708a0809a76cf6afa795e17c4d16bcaaad5c
Simon Weizman <simon@weizman.us>
2021-02-28 04:58:48 -0500

    Added blank testcase for Events endpoints

---
+++ backend/python/tests/routes/test_events.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+import json
+from src.models.event import Event
+from tests.base import BaseTestCase
+
+
+class TestEventsBlueprint(BaseTestCase):
+    """Tests for the Events Endpoints"""
+    pass
