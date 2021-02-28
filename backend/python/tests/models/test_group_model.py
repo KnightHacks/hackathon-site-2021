3c4add0687ed204b7ab675b66bf941c76579be1a
Simon Weizman <simon@weizman.us>
2021-02-28 08:55:07 -0500

    Added testcase for group.to_json

--- backend/python/tests/models/test_group_model.py
+++ backend/python/tests/models/test_group_model.py
@@ -28,3 +28,31 @@ class TestGroupModel(BaseTestCase):
         self.assertEqual(group.categories, ["cat1"])
         self.assertEqual(group.date, now)
         self.assertEqual(group.members[0], hacker)
+
+    def test_group_to_json(self):
+        hacker = Hacker.createOne(username="foobar",
+                                  email="foobar@email.com",
+                                  password="password",
+                                  first_name="foo",
+                                  last_name="bar",
+                                  roles=("HACKER",))
+
+        now = datetime.now()
+        group = Group.createOne(name="foobar",
+                                icon="image",
+                                categories=["cat1"],
+                                date=now,
+                                members=[hacker])
+
+        group_json = group.to_json()
+
+        self.assertEqual("foobar", group_json["name"])
+        self.assertEqual("image", group_json["icon"])
+        self.assertEqual(["cat1"], group_json["categories"])
+        self.assertEqual(now, group_json["date"])
+        self.assertIn({
+            "first_name": hacker.first_name,
+            "last_name": hacker.last_name,
+            "email": hacker.email,
+            "username": hacker.username
+        }, group_json["members"])
