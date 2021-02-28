ff95e2fe72f6bd3229a3cc001c95d0e7f9223efd
Simon Weizman <simon@weizman.us>
2021-02-28 08:45:20 -0500

    Added testcase for user.findOne

--- backend/python/tests/models/test_user_model.py
+++ backend/python/tests/models/test_user_model.py
@@ -17,3 +17,16 @@ class TestUserModel(BaseTestCase):
         self.assertEqual(user.username, "foobar")
         self.assertEqual(user.email, "foobar@email.com")
         self.assertTrue(user.password)
+
+    def test_findOne_user(self):
+        User.createOne(username="foobar",
+                       email="foobar@email.com",
+                       password="password",
+                       roles=("HACKER",))
+
+        user = User.findOne(username="foobar")
+
+        self.assertIsNone(user.id)
+        self.assertEqual(user.username, "foobar")
+        self.assertEqual(user.email, "foobar@email.com")
+        self.assertTrue(user.password)
