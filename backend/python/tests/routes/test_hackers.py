447c9fc2f366f1e423906559d5b0187f1f85b2a6
Simon Weizman <simon@weizman.us>
2021-03-03 21:53:50 -0500

    Finished hacker api coverage

--- backend/python/tests/routes/test_hackers.py
+++ backend/python/tests/routes/test_hackers.py
@@ -117,3 +117,58 @@ class TestHackersBlueprint(BaseTestCase):
         self.assertEqual(res.status_code, 404)
         self.assertIn(
             "The specified hacker does not exist in the database.", data["description"])
+
+    """update_hacker"""
+    def test_update_user_profile_settings(self):
+        Hacker.createOne(username="foobar",
+                         email="foobar@email.com",
+                         password="123456",
+                         roles=ROLES.HACKER)
+
+        res = self.client.put(
+            "/api/hackers/foobar/",
+            data=json.dumps({
+                "email": "schmuckbar@mensch.com"
+            }),
+            content_type="application/json")
+
+        data = json.loads(res.data.decode())
+
+        self.assertEqual(res.status_code, 201)
+
+        updated = Hacker.findOne(username="foobar")
+
+        self.assertEqual(updated.email, "schmuckbar@mensch.com")
+
+    def test_update_user_profile_settings_invalid_json(self):
+        res = self.client.put(
+            "/api/hackers/foobar/",
+            data=json.dumps({}),
+            content_type="application/json")
+
+        self.assertEqual(res.status_code, 400)
+
+    def test_update_user_profile_settings_not_found(self):
+        res = self.client.put(
+            "/api/hackers/foobar/",
+            data=json.dumps({
+                "email": "schmuckbar@mensch.com"
+            }),
+            content_type="application/json")
+
+        self.assertEqual(res.status_code, 404)
+
+    def test_update_user_profile_settings_invalid_datatypes(self):
+        Hacker.createOne(username="foobar",
+                         email="foobar@email.com",
+                         password="123456",
+                         roles=ROLES.HACKER)
+
+        res = self.client.put(
+            "/api/hackers/foobar/",
+            data=json.dumps({
+                "email": 2
+            }),
+            content_type="application/json")
+
+        self.assertEqual(res.status_code, 400)
