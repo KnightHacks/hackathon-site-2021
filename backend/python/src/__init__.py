4d60d0df1a6b59515341a679ae336d6e1a35e9c0
Simon Weizman <simon@weizman.us>
2021-03-07 18:47:48 -0500

    Added auth blueprint to init

--- backend/python/src/__init__.py
+++ backend/python/src/__init__.py
@@ -104,6 +104,7 @@ def create_app():
     from src.api.club_events import club_events_blueprint
     from src.api.categories import categories_blueprint
     from src.api.email_verification import email_verify_blueprint
+    from src.api.auth import auth_blueprint
 
     app.register_blueprint(hackers_blueprint, url_prefix="/api")
     app.register_blueprint(stats_blueprint, url_prefix="/api")
@@ -113,6 +114,7 @@ def create_app():
     app.register_blueprint(club_events_blueprint, url_prefix="/api")
     app.register_blueprint(categories_blueprint, url_prefix="/api")
     app.register_blueprint(email_verify_blueprint, url_prefix="/api")
+    app.register_blueprint(auth_blueprint, url_prefix="/api")
 
     """Register Error Handlers"""
     from src.common import error_handlers
