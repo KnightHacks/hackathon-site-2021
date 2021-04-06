fc8c62687b6aa4e9af96692ba1594652d7741d0d
Abraham Hernandez <abeherham@knights.ucf.edu>
2021-04-05 22:42:20 -0400

    Adds social media icons with links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import { CgMenu, CgVolume } from "react-icons/cg";
-function App() {
+import { FaTwitter, FaInstagram, FaFacebookF } from "react-icons/fa";
+
+const App = () => {
   return (
     <div className="bg-koi-fish-pond w-screen h-screen grid grid-cols-5 grid-rows-1 grid-flow-col gap-0">
       <div className="col-span-1 w-full">
@@ -11,18 +13,31 @@ function App() {
 
       <div className="col-span-3 text-white w-full bg-landing-transparent">
         <div className="flex justify-center items-center flex-col w-full h-full">
-          <p className="text-5xl">Welcome to</p>
-          <p className="text-8xl">KNIGHT HACKS</p> {/* change this */}
-          <p className="text-xl">October 9th - October 11th, 2021</p>
-          <button className="px-4 py-2 border-white border-8 rounded-full text-4xl mt-8 hover:bg-blue-400 focus:outline-none">
-            REGISTER
-          </button>
+          <div className="flex justify-center items-center flex-col w-full h-full">
+            <p className="text-5xl">Welcome to</p>
+            <p className="text-8xl">KNIGHT HACKS</p> {/* change this */}
+            <p className="text-xl">October 9th - October 11th, 2021</p>
+            <button className="px-4 py-2 border-white border-8 rounded-full text-4xl mt-8 hover:bg-blue-400 focus:outline-none">
+              REGISTER
+            </button>
+          </div>
+          <div className="flex flex-row mt-auto mb-8 space-x-8 text-6xl">
+            <a href="https://twitter.com/KnightHacks?lang=en/">
+              <FaTwitter className="border-4 border-white rounded-xl p-2 hover:bg-blue-400" />
+            </a>
+            <a href="https://www.instagram.com/knighthacks/">
+              <FaInstagram className="border-4 border-white rounded-xl p-2 hover:bg-blue-400" />
+            </a>
+            <a href="https://www.facebook.com/KnightHacks/">
+              <FaFacebookF className="border-4 border-white rounded-xl p-2 hover:bg-blue-400" />
+            </a>
+          </div>
         </div>
       </div>
 
       <div className="col-span-1 w-full" />
     </div>
   );
-}
+};
 
 export default App;
