79062d94edbf5e195c85b6184c71674bae34bd48
Abraham Hernandez <abeherham@knights.ucf.edu>
2021-04-05 20:04:27 -0400

    Sets up basic landing page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,18 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "koi-fish-pond": "url('/src/assets/KoiFishPond.png')",
+      }),
+      backgroundColor: (theme) => ({
+        "test-10": "rgba(191, 219, 254, 0.2)",
+      }),
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
