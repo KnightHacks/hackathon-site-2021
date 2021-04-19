7747273c73f79b7edffa3a592b1b73f90545900e
Robert Boyd III <rob@robertcboyd.com>
2021-04-19 13:44:36 -0400

    Add blur-up transition on page change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import "./App.css";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import About from "./pages/About";
@@ -6,18 +8,36 @@ import Schedule from "./pages/Schedule";
 import FAQ from "./pages/FAQ";
 import Register from "./pages/Register";
 
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { useLocation } from "react-router-dom";
+
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route path="/about" component={About} />
-        <Route path="/sponsors" component={Sponsors} />
-        <Route path="/schedule" component={Schedule} />
-        <Route path="/faq" component={FAQ} />
-        <Route path="/register" component={Register} />
+        <Route path="*">
+          <AppWithTransitions />
+        </Route>
       </Switch>
     </Router>
+  );
+};
+
+const AppWithTransitions = () => {
+  const location = useLocation();
+  return (
+    <TransitionGroup>
+      <CSSTransition key={location.key} classNames="fade" timeout={1000}>
+        <Switch location={location}>
+          <Route exact path="/" component={Landing} />
+          <Route path="/about" component={About} />
+          <Route path="/sponsors" component={Sponsors} />
+          <Route path="/schedule" component={Schedule} />
+          <Route path="/faq" component={FAQ} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </CSSTransition>
+    </TransitionGroup>
   );
 };
 
