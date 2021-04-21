00c11c8e60b8baf5738e93cbfd72f1180979f238
Robert Boyd III <rob@robertcboyd.com>
2021-04-21 00:49:44 -0400

    Add heading to sponsors page

--- frontend/src/pages/Sponsors.js
+++ frontend/src/pages/Sponsors.js
@@ -1,5 +1,21 @@
+import Page from "../components/Page";
+
 const Sponsors = () => {
-  return <div>Sponsors Page</div>;
+  return (
+    <Page onLanding={false}>
+      <h1
+        className={`
+          text-4xl w-full text-center mt-16
+          md:text-5xl md:mt-24
+          lg:mt-32
+          xl:text-6xl xl:mt-36
+          2xl:text-7xl 2xl:mt-36
+        `}
+      >
+        Our Sponsors
+      </h1>
+    </Page>
+  );
 };
 
 export default Sponsors;
