e32914ccbcb4b23a26dd5be82418b8402987cd07
Ro-Hanna Jowallah <rohannajowallah@gmail.com>
2021-04-23 18:57:02 -0400

    Title Update

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,5 +1,15 @@
+import { FaTwitter, FaInstagram, FaFacebookF } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import Page from "../components/Page";
 const About = () => {
-  return <div>About Page</div>;
+  return (
+    <Page onLanding={false}>
+      <div className="flex justify-start items-center w-full flex-col my-4 md:my-12">
+        <h1 className="text-4xl sm:text-4xl md:text-6xl">About Knights Hack</h1>
+        <div className="my-4 flex flex-col items-center w-2/3">Hello</div>
+      </div>
+    </Page>
+  );
 };
 
 export default About;
