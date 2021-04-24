6977c78b9bd262d0f150235842b1fcbaed5ef57f
Ro-Hanna Jowallah <rohannajowallah@gmail.com>
2021-04-23 20:40:46 -0400

    Update About.js

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,13 +1,18 @@
 import { FaTwitter, FaInstagram, FaFacebookF } from "react-icons/fa";
-import { Link } from "react-router-dom";
 import Page from "../components/Page";
+/**
+ * @desc About Page
+ * @author Ro-Hanna Jowallah
+ */
 const About = () => {
   return (
     <Page onLanding={false}>
       <div className="flex justify-start items-center w-full flex-col my-4 md:my-12">
-        <h1 className="text-4xl sm:text-4xl md:text-8xl">About Knights Hack</h1>
-        <div className="my-6 flex flex-col items-center w-2/3">
-          <div className="text-left font-light sm:text-lg  mb-4 text-xl sm:text-xl md:text-2xl mt-36">
+        <h1 className="text-4xl sm:text-4xl mt-20 md:text-6xl text-center">
+          About Knights Hacks
+        </h1>
+        <div className="my-2 flex flex-col items-center w-2/3">
+          <div className="text-left font-extralight sm:text-lg  mb-2 text-lg sm:text-lg md:text-xl lg:text-2xl mt-24">
             Connect Collaborate and Create with 700 of the brightest developers,
             engineers, and designers in the south-east. Whether you’re a
             seasoned hacker or a tech newbie, Knight Hacks welcomes you. Just
@@ -15,6 +20,17 @@ const About = () => {
             care of the rest. Create a product, learn new skills and have fun
             with friends old and new – all in 36 hours.
           </div>
+        </div>
+        <div className="flex flex-row  mb-8 space-x-8  mt-32 text-4xl sm:text-5xl md:text-6xl">
+          <a href="https://twitter.com/KnightHacks?lang=en/">
+            <FaTwitter className="border-4 border-white rounded-xl p-2 hover:bg-blue-400" />
+          </a>
+          <a href="https://www.instagram.com/knighthacks/">
+            <FaInstagram className="border-4 border-white rounded-xl p-2 hover:bg-blue-400" />
+          </a>
+          <a href="https://www.facebook.com/KnightHacks/">
+            <FaFacebookF className="border-4 border-white rounded-xl p-2 hover:bg-blue-400" />
+          </a>
         </div>
       </div>
     </Page>
